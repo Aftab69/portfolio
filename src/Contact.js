1d42c4007508e5aabf8d14269e62729dfdeac93f
Aftab69 <aftabhssn3@gmail.com>
2022-08-04 03:12:31 +0530

    contact page done

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import "./Contact.css";
 
-const Contact = () => {
+function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <>
+      <div className='contactpageContainer' id='contact'>
+        <div className='contactpageHeading'>
+          <p>-: Contact Me :-</p> 
+        </div>
+        <div className='formContainerBox'>
+          <div className='formContainer'>
+              <input type="text" placeholder='Enter your name here' />
+              <input type="email" placeholder='Enter a valid email address' />
+              <textarea type="text" placeholder='Enter your message' />
+              <button>Send message</button>
+          </div> 
+        </div>
+        <div className='infoContainer'>
+          {/* Might add information later */}
+        </div>
+      </div>
+    </>
+    
+  );
 }
 
 export default Contact
