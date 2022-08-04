3c872f3bb6cb606c8953d113cbe80e4d7999a133
Aftab69 <aftabhssn3@gmail.com>
2022-08-04 13:31:18 +0530

    configured emailjs

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import emailjs from "@emailjs/browser";
+
 import "./Contact.css";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8pat0om', 'template_ui3wpc9', form.current, 'ZiJUUW6UQfngNLJ73')
+      .then((result) => {
+          console.log(result.text);
+          window.alert("Message sent successfully");
+      }, (error) => {
+          console.log(error.text);
+          window.alert("Error");
+      });
+  };
   return (
     <>
       <div className='contactpageContainer' id='contact'>
@@ -9,12 +25,12 @@ function Contact() {
           <p>-: Contact Me :-</p> 
         </div>
         <div className='formContainerBox'>
-          <div className='formContainer'>
-              <input type="text" placeholder='Enter your name here' />
-              <input type="email" placeholder='Enter a valid email address' />
-              <textarea type="text" placeholder='Enter your message' />
-              <button>Send message</button>
-          </div> 
+          <form ref={form} onSubmit={sendEmail} className='formContainer'>
+              <input type="text" name="user_name" placeholder='Enter your name here' required />
+              <input type="email" name="user_email" placeholder='Enter a valid email address' required />
+              <textarea type="text" name="message" placeholder='Enter your message' required />
+              <button type="submit" value="Send">Send message</button>
+          </form> 
         </div>
         <div className='infoContainer'>
           {/* Might add information later */}
