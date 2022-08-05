71525c8a1105b70a3be801283adb504ce15cb13e
Aftab69 <aftabhssn3@gmail.com>
2022-08-06 02:50:11 +0530

    contact page enhanced

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 import emailjs from "@emailjs/browser";
 
 import "./Contact.css";
@@ -20,26 +20,24 @@ function Contact() {
       e.target.reset();
   };
   return (
-    <>
-      <div className='contactpageContainer' id='contact'>
-        <div className='contactpageHeading'>
-          <p>-: Contact Me :-</p> 
-        </div>
-        <div className='formContainerBox'>
-          <form ref={form} onSubmit={sendEmail} className='formContainer'>
-              <input type="text" name="user_name" placeholder='Enter your name here' required />
-              <input type="email" name="user_email" placeholder='Enter a valid email address' required />
-              <textarea type="text" name="message" placeholder='Enter your message' required />
-              <button type="submit" value="Send">Send message</button>
-          </form> 
-        </div>
-        <div className='infoContainer'>
-          {/* Might add information later */}
-        </div>
-      </div>
-    </>
-    
+      <>
+       <div id='contact' className='contactpageContainer'>
+           <div className='contactpageHeading'>
+                <p>-: Contact Me :-</p>
+           </div>
+           <div className='contactformContainer'>
+               <form ref={form} onSubmit={sendEmail} className='formContainer'>
+                  <input type="text" name="user_name" placeholder='Enter your name' required />
+                  <input type="email" name="user_email" placeholder='Enter a valid email address' required />
+                  <textarea type="text" name="message" placeholder='Enter your message' required />
+                  <button type="submit" value="Send">Submit</button>
+               </form>
+           </div>
+           <div className='contactpageFooterElements'>
+           </div>
+       </div>
+      </>
   );
 }
 
-export default Contact
+export default Contact;
