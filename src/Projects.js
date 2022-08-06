7e24377437283405930f3d4b38f841b6cdc8c5c1
Aftab69 <aftabhssn3@gmail.com>
2022-08-06 14:26:21 +0530

    projects page optimized for mobile

--- src/Projects.js
+++ src/Projects.js
@@ -14,6 +14,11 @@ const Projects = () => {
           <Card cardImage={image1} projectName="Project3" projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
           <Card cardImage={image1} projectName="Project4" projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
           <Card cardImage={image1} projectName="Project5" projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
+          <Card cardImage={image1} projectName="Project5" projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
+          <Card cardImage={image1} projectName="Project5" projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
+          <Card cardImage={image1} projectName="Project5" projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
+          <Card cardImage={image1} projectName="Project5" projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
+          <Card cardImage={image1} projectName="Project5" projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
         </div>
       </div>
     </>
