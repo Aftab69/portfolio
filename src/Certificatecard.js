f8ddda528eb9275a6d31e0a3a9ec878cd1e60776
Aftab69 <aftabhssn3@gmail.com>
2022-08-08 11:56:44 +0530

    certificate page card popup done

--- src/Certificatecard.js
+++ src/Certificatecard.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 import "./Certificatecard.css";
 
 const Certificatecard = (props) => {
+  const [divstyle, setDivstyle] = useState({display:"none"})
+  const handleclick = () =>{
+      setDivstyle({display:"block"})
+  }
+  const handlebutton = () =>{
+    setDivstyle({display:"none"})
+  }
+
   return (
     <>
         <div className='certificateCardContainer'>
-            <img src={props.cardImage} alt={props.cardImage} />
+            <img onClick={handleclick} src={props.cardImage} alt={props.cardImage} />
             <p>{props.cardData}</p>
+        </div>
+        <div className='popup' style={divstyle}>
+          <div className='popupContainer'>
+            <button onClick={handlebutton}>X</button>
+            <img src={props.cardImage} alt={props.cardImage} />
+          </div>
         </div>
     </>
   )
