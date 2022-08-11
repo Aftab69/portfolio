591109842ac46145e474aa54f345d49c5b24043e
Aftab69 <aftabhssn3@gmail.com>
2022-08-12 02:40:04 +0530

    card images optimized

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Card.css";
 
 const Card = (props) => {
+  const [divstyle, setDivstyle] = useState({display:"none"})
+  const handleclick = () =>{
+      setDivstyle({display:"block"})
+  }
+  const handlebutton = () =>{
+    setDivstyle({display:"none"})
+  }
   return (
     <>
       <div className='cardContainer'>
          <h3>{props.projectName}</h3>
-         <img src={props.cardImage} alt={props.cardImage} />
+         <img onClick={handleclick} src={props.cardImage} alt={props.cardImage} />
          <p>{props.projectDescription}</p>
          <div className='cardButtons'>
             <button><a href={props.linkcode} target="_blank" rel="noreferrer">Code</a></button>
             <button><a href={props.linkapp} target="_blank" rel="noreferrer">App</a></button>
          </div>
       </div>
+      <div className='popup' style={divstyle}>
+          <div className='popupContainer'>
+            <button onClick={handlebutton}>X</button>
+            <img src={props.cardImage} alt={props.cardImage} />
+          </div>
+        </div>
     </>
   )
 }
