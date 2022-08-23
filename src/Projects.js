a1998d19958e9596effcbe38ce7f146b191560fe
Aftab69 <aftabhssn3@gmail.com>
2022-08-23 11:46:05 +0530

    project 3 added

--- src/Projects.js
+++ src/Projects.js
@@ -3,12 +3,15 @@ import "./Projects.css";
 import Card from './Card';
 import image1 from "../src/Images/project1image.png";
 import image2 from "../src/Images/project2image.png";
+import image3 from "../src/Images/project3image.png";
 
 const Projects = () => {
   const link1code = "https://github.com/Aftab69/movie-generator";
   const link1app = "https://moviegeneratorsite.netlify.app/"
   const link2code = "https://github.com/Aftab69/portfolio";
   const link2app = "https://aftabhussain.netlify.app/";
+  const link3code = "https://github.com/Aftab69/authentication-app";
+  const link3app = "https://quotezapp.herokuapp.com/";
 
   return (
     <>
@@ -17,7 +20,7 @@ const Projects = () => {
         <div className='elementsContainerProjectsPage'>
           <Card linkcode={link1code} linkapp={link1app} cardImage={image1} projectName="Movie Generator" projectDescription="An app that generates random movies, lists out the popular as well as the trending movies, along with the search movie feature." />
           <Card linkcode={link2code} linkapp={link2app} cardImage={image2} projectName="Portfolio" projectDescription="This website is being built out of HTML, CSS, Javascript, ReactJS and EmailJS." />
-          <Card linkcode={link1code} linkapp={link1app} cardImage={image1} projectName="Movie Generator" projectDescription="An app that generates random movies, lists out the popular as well as the trending movies, along with the search movie feature." />
+          <Card linkcode={link3code} linkapp={link3app} cardImage={image3} projectName="Quotezapp" projectDescription="An app that generates random movies, lists out the popular as well as the trending movies, along with the search movie feature." />
           <Card linkcode={link2code} linkapp={link2app} cardImage={image2} projectName="Portfolio" projectDescription="This website is being built out of HTML, CSS, Javascript, ReactJS and EmailJS." />
         </div>
       </div>
